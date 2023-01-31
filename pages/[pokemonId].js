4c577c41517757d4f5c3c659f0d2946bdc7e38fe
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-01-31 23:35:25 +0600

    readme file added

--- pages/[pokemonId].js
+++ pages/[pokemonId].js
@@ -1,11 +1,8 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
-import Image from "next/image";
-import Link from "next/link";
 import PokemonDetails from "@/Components/PokemonDetails";
 export default function pokemon() {
   return (
-    <div className="">
+    <div >
       <Head>
         <title>Pokemon</title>
       </Head>
